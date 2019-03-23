3b95cec2db8e704a905ebc68ac41f2c4ecb423ff
SaraMousa <saramousa@cmail.carleton.ca>
2019-03-22 23:52:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved
 app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
 //routes
-app.get('/index', function(req,res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/player1', function(req,res) {
+    res.sendFile(__dirname + '/public/player1.html');
 });
 
 //app.get('/controller', function(req,res) {
@@ -35,12 +35,14 @@ socketIO.on('connection', function(socket) {
 
     socket.on('green', function(data) {
         console.log('green event heard');
-        socketIO.sockets.emit('color_change', {r:0, g:255, b:0});
+        socketIO.sockets.emit('color_change2', {r:0, g:255, b:0});
     });
 
     socket.on('blue', function(data) {
         console.log('blue event heard');
-        socketIO.sockets.emit('color_change', {r:25, g:3, b:255});
+        socketIO.sockets.emit('color_change', {r:0, g:0, b:255});
+
+
     });
 });
 
