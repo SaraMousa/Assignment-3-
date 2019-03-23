1822825c3ad661beafc08a0537baaa391f1ab333
SaraMousa <saramousa@cmail.carleton.ca>
2019-03-22 23:50:34 -0400

    Create remove.js

---
+++ public/remove.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent('remove', {
+    schema: {},
+    init : function() {
+        const Context_AF = this;
+
+        Context_AF.el.addEventListener('click', function(event) {
+            console.log("click");
+           
+            Context_AF.deleteMyself();
+           
+        });
+    },
+    deleteMyself : function() {
+        const Context_AF = this;
+        Context_AF.el.parentNode.removeChild(Context_AF.el); 
+    }
+});
